888543fb8f98608507dfc2fdd2d90ef2f62619c7
Daniel Lopez <d@niel.to>
2025-07-03 08:05:36 -0700

    feat: add sword

---
+++ src/components/sword.tsx
@@ -0,0 +1,150 @@
+import CommandPalette from '@/components/command-palette'
+import Button from '@/components/ui/button'
+import books, { booksAndChaptersMap } from '@/lib/books'
+import useLocalStorage from '@/lib/useLocalStorage'
+import copyToClipboard from '@/lib/copyToClipboard'
+
+type HistoryEntry = {
+  chapterLink: string
+  bookChapter: string
+}
+
+export default function Sword({
+  excludeCommandPalette,
+  onClick,
+}: {
+  excludeCommandPalette?: boolean
+  onClick?: (args?: unknown) => void
+}) {
+  const [swordText, setSwordText] = useLocalStorage('sword-text', '1:1')
+  const [history, setHistory] = useLocalStorage<HistoryEntry[]>(
+    'sword-history',
+    []
+  )
+
+  const bookNumber = swordText?.split(':')[0]
+  const bookChapter = swordText?.split(':')[1]
+  const bookName = swordText
+    ? books[Number(swordText.split(':')[0]) - 1]
+    : undefined
+  const bookChapters = bookName ? (booksAndChaptersMap[bookName] ?? 1) : 1
+  const verse = '001'
+  const bibleText = `${bookNumber}${(bookChapter ?? '').padStart(3, '0')}${verse}`
+
+  const chapterLink = `https://www.jw.org/finder?srcid=jwlshare&wtlocale=E&prefer=lang&bible=${bibleText}&pub=nwtsty`
+  const bookWithChapter = `${bookName} ${bookChapter}`
+  return (
+    <div className='flex flex-grow flex-col space-y-4'>
+      <ul className='flex-grow space-y-4'>
+        {[].map(({ chapterLink, bookChapter }) => (
+          <li key={chapterLink}>
+            <Button
+              onClick={() => {
+                window.open(chapterLink)
+              }}
+            >
+              {bookChapter}
+            </Button>
+          </li>
+        ))}
+      </ul>
+      <a
+        className='block w-full translate-y-[-4px] transform rounded-lg bg-[#5a3e84] p-3 text-center text-lg duration-[600ms] ease-[cubic-bezier(.3,.7,.4,1)] group-hover:translate-y-[-6px] group-hover:duration-[250ms] group-active:translate-y-[-2px] group-active:duration-[34ms] hover:ease-[cubic-bezier(.3,.7,.4,1.5)] disabled:pointer-events-none disabled:opacity-25'
+        href={chapterLink}
+        target='_blank'
+        onClick={() => {
+          copyToClipboard(bookWithChapter)
+          if (onClick) {
+            onClick(bookWithChapter)
+          }
+          setHistory([
+            ...history,
+            {
+              chapterLink,
+              bookChapter: bookWithChapter,
+            },
+          ])
+        }}
+      >
+        {bookWithChapter}
+      </a>
+      <div className='flex'>
+        <select
+          className='bg-cobalt w-full p-4'
+          value={swordText?.split(':')[0]}
+          onChange={e => {
+            const newLookupText = `${e.target.value}:1`
+            setSwordText(newLookupText)
+          }}
+        >
+          {books.map((book, idx) => (
+            <option key={idx + 1} value={idx + 1}>
+              {book}
+            </option>
+          ))}
+        </select>
+        <select
+          className='bg-cobalt w-full p-4'
+          value={swordText?.split(':')[1]}
+          onChange={e => {
+            const [bookNumber] = swordText.split(':')
+            const newSequence = `${bookNumber}:${e.target.value}`
+            setSwordText(newSequence)
+          }}
+        >
+          {Array.from(
+            {
+              length: bookChapters,
+            },
+            (_, i) => i + 1
+          ).map(ch => (
+            <option key={ch} value={ch}>
+              {ch}
+            </option>
+          ))}
+        </select>
+      </div>
+      {!excludeCommandPalette && (
+        <CommandPalette
+          commands={[
+            {
+              id: `go-text`,
+              title: `go text`,
+              action: () => {
+                setHistory([
+                  ...history,
+                  {
+                    chapterLink,
+                    bookChapter: bookWithChapter,
+                  },
+                ])
+                window.open(chapterLink, '_blank')
+              },
+            },
+            ...books.map((book, index) => ({
+              id: `lookup-${book}`,
+              title: `lookup ${book}`,
+              action: () => {
+                setSwordText(`${index + 1}:1`)
+              },
+            })),
+            ...Array.from(
+              {
+                length: bookChapters,
+              },
+              (_, i) => i + 1
+            ).map(bookChapter => {
+              return {
+                id: `lookup-${bookName}-${bookChapter}`,
+                title: `lookup ${bookName} ${bookChapter}`,
+                action: () => {
+                  setSwordText(`${bookNumber}:${bookChapter}`)
+                },
+              }
+            }),
+          ]}
+        />
+      )}
+    </div>
+  )
+}
--- src/frontend/routes/home.tsx
+++ src/frontend/routes/home.tsx
@@ -1,10 +1,12 @@
+import Sword from '@/components/sword'
 import { Main, Title } from '@/components/ui'
 
 export default function Home() {
   return (
     <Main className='flex flex-col p-4'>
-      <div className='flex flex-grow flex-col items-center justify-center space-y-4'>
+      <div className='flex flex-grow flex-col space-y-4'>
         <Title>s4 ⚔️</Title>
+        <Sword />
       </div>
     </Main>
   )
