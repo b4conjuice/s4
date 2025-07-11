3ac7da7609387c9bddd5b779c92f3cf58810d266
Daniel Lopez <d@niel.to>
2025-07-11 15:49:47 -0700

    refactor(api/sword): directly request data instead of through api

---
+++ src/lib/requestMeetingLinks.ts
@@ -0,0 +1,50 @@
+import * as cheerio from 'cheerio'
+
+const MEETING_LINKS_URL = 'https://wol.jw.org/en/wol/meetings/r1/lp-e/'
+
+export default async function requestMeetingLinks(date: string) {
+  const url = `${MEETING_LINKS_URL}${date}`
+  try {
+    const response = await fetch(url)
+
+    if (!response.ok) {
+      throw new Error(
+        `Failed to fetch ${url}: ${response.status} ${response.statusText}`
+      )
+    }
+
+    const html = await response.text()
+    const $ = cheerio.load(html)
+
+    const meetingLinkSelector = `.todayItems > .todayItem > .itemCaption > .linkCard > a.jwac`
+    const meetingLinkElements = $(meetingLinkSelector)
+    if (meetingLinkElements) {
+      const weekElement = $(
+        '.todayItems > .todayItem:nth-child(1) > .itemData h1 strong'
+      )
+      const week = weekElement.text()
+      const mwElement = $(
+        '.todayItems > .todayItem:nth-child(1) > .itemCaption > .linkCard > a.jwac'
+      )
+      const mwReading = $(
+        '.todayItems > .todayItem:nth-child(1) > .itemData > header > h2'
+      )
+      const wtElement = $('.todayItems > .todayItem:nth-child(2) > .itemData a')
+      const baseUrl = 'https://wol.jw.org'
+      return {
+        success: true,
+        date,
+        url,
+        week,
+        mw: `${baseUrl}${mwElement.attr('href')}`,
+        mwTitle: mwReading.text(),
+        wt: `${baseUrl}${wtElement.attr('href')}`,
+        wtTitle: wtElement.text(),
+      }
+    } else {
+      throw new Error(`Could not find meetingLinkElements for date ${date}`)
+    }
+  } catch (error) {
+    console.error('Error: ', error)
+  }
+}
--- src/server/api/routers/sword.ts
+++ src/server/api/routers/sword.ts
@@ -1,43 +1,46 @@
 import { z } from 'zod'
 
 import { createTRPCRouter, publicProcedure } from '@/server/api/trpc'
-import fetcher from '@/lib/fetcher'
+import requestMeetingLinks from '@/lib/requestMeetingLinks'
+import requestDailyText from '@/lib/requestDailyText'
 
-type MWTResponse = {
-  success: boolean
-  date: string
-  url: string
-  week: string
-  mw: string
-  mwTitle: string
-  wt: string
-  wtTitle: string
-}
+type MWTResponse =
+  | {
+      success: boolean
+      date: string
+      url: string
+      week: string
+      mw: string
+      mwTitle: string
+      wt: string
+      wtTitle: string
+    }
+  | undefined
 
-type DTResponse = {
-  comment: string
-  dailyText: string
-  date: string
-  scripture: string
-  success: boolean
-  text: string
-}
+type DTResponse =
+  | {
+      comment: string
+      dailyText: string
+      date: string
+      scripture: string
+      success: boolean
+      text: string
+    }
+  | undefined
 
 export const swordRouter = createTRPCRouter({
   mwt: publicProcedure
     .input(z.object({ date: z.string() }))
     .query(async ({ input }) => {
-      const data = await fetcher<MWTResponse>(
-        `https://api.dlopez.app/api/sword/mwt/${input.date}`
-      )
+      const date = String(input.date).replace(/-/g, '/')
+      const data: MWTResponse = await requestMeetingLinks(date)
       return data
     }),
   dt: publicProcedure
     .input(z.object({ date: z.string() }))
     .query(async ({ input }) => {
-      const data: DTResponse = await fetcher(
-        `https://api.dlopez.app/api/sword/dt/${input.date}`
-      )
+      const date = String(input.date).replace(/-/g, '/')
+      const data: DTResponse = await requestDailyText(date)
       return data
     }),
 })
