06ecebcb57edb68784505463ed7cfb50715f4305
Daniel Lopez <d@niel.to>
2025-07-27 16:42:08 -0700

    chore: remove unused post component and db table

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -20,20 +20,6 @@ import {
  */
 export const createTable = pgTableCreator(name => `s4_${name}`)
 
-export const posts = createTable(
-  'post',
-  d => ({
-    id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
-    name: d.varchar({ length: 256 }),
-    createdAt: d
-      .timestamp({ withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
-  }),
-  t => [index('name_idx').on(t.name)]
-)
-
 export const notes = pgTable('n4_note', {
   id: serial('id').primaryKey(),
   text: varchar('text').notNull(),
