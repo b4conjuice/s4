765507f044dcc906c82b71bee17994af00248bfe
Daniel Lopez <d@niel.to>
2025-08-07 17:21:11 -0700

    fix(notes-page): fetch notes only when signed in, otherwise display sign in message

--- src/frontend/routes/notes.tsx
+++ src/frontend/routes/notes.tsx
@@ -1,5 +1,6 @@
 import { NavLink as Link } from 'react-router'
 import { PencilSquareIcon } from '@heroicons/react/20/solid'
+import { SignedIn, SignedOut } from '@clerk/nextjs'
 
 import { Main } from '@/components/ui'
 import Menu from '@/components/menu'
@@ -8,14 +9,26 @@ import NoteList from '@/components/note-list'
 import { api } from '@/trpc/react'
 import NoteListSkeleton from '@/components/note-list-skeleton'
 
-export default function Notes() {
+function Notes() {
   const { data: notes, isFetching } = api.note.getAll.useQuery()
+  if (isFetching) {
+    return <NoteListSkeleton />
+  }
+  return <NoteList notes={notes ?? []} />
+}
+
+export default function NotesPage() {
   return (
     <>
       <TopNav title='notes' />
       <Main className='flex flex-col px-2'>
         <div className='flex flex-grow flex-col space-y-4'>
-          {isFetching ? <NoteListSkeleton /> : <NoteList notes={notes ?? []} />}
+          <SignedIn>
+            <Notes />
+          </SignedIn>
+          <SignedOut>
+            <p>sign in to view your notes</p>
+          </SignedOut>
         </div>
       </Main>
       <footer className='bg-cb-dusty-blue sticky bottom-0 flex items-center justify-between px-2 pt-2 pb-6'>
