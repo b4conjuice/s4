c4717ecb0185461123ae680cbb21ba8131d35cf2
Daniel Lopez <d@niel.to>
2025-08-09 18:16:38 -0700

    feat(text-page): add book-search to navigate to books/text pages

--- src/frontend/routes/text.tsx
+++ src/frontend/routes/text.tsx
@@ -1,10 +1,10 @@
-import { NavLink as Link, useParams } from 'react-router'
+import { NavLink as Link, useNavigate, useParams } from 'react-router'
 import { PencilSquareIcon } from '@heroicons/react/20/solid'
 
 import { Main, Title } from '@/components/ui'
 import {
-  booksAndChaptersMap,
   getScriptureUrl,
+  transformScripturetoText,
   transformTextToScripture,
 } from '@/lib/books'
 import { api } from '@/trpc/react'
@@ -12,9 +12,11 @@ import NoteListSkeleton from '@/components/note-list-skeleton'
 import NoteList from '@/components/note-list'
 import Menu from '@/components/menu'
 import BookMenu from '@/components/book-menu'
+import BookSearch from '@/components/book-search'
 
 export default function Text() {
   const { text } = useParams()
+  const navigate = useNavigate()
   if (!text) {
     return <Main className='flex flex-col p-4'>`text` is required</Main>
   }
@@ -30,7 +32,6 @@ export default function Text() {
     text,
   })
   const scriptureUrl = getScriptureUrl(text)
-  const chapters = booksAndChaptersMap[scripture.bookName] ?? 1
   return (
     <>
       <Main className='flex flex-col p-4'>
@@ -53,18 +54,32 @@ export default function Text() {
           </div>
         </div>
       </Main>
-      <footer className='bg-cb-dusty-blue sticky bottom-0 flex items-center justify-between px-2 pt-2 pb-6'>
-        <div className='flex space-x-4'>
-          <Menu />
-          <BookMenu />
-        </div>
-        <div className='flex space-x-4'>
-          <Link
-            className='text-cb-yellow hover:text-cb-yellow/75 disabled:pointer-events-none disabled:opacity-25'
-            to='new'
-          >
-            <PencilSquareIcon className='h-6 w-6' />
-          </Link>
+      <footer className='bg-cb-dusty-blue sticky bottom-0 flex flex-col space-y-2 px-2 pt-2 pb-6'>
+        <BookSearch
+          onSelectBook={async scripture => {
+            const { bookNumber, chapter, verse } = scripture
+            if (verse) {
+              const text = transformScripturetoText(scripture)
+              await navigate(`/text/${text}`)
+            } else {
+              await navigate(`/books/${bookNumber}/${chapter}`)
+            }
+          }}
+          showRecentCommands
+        />
+        <div className='flex items-center justify-between'>
+          <div className='flex space-x-4'>
+            <Menu />
+            <BookMenu />
+          </div>
+          <div className='flex space-x-4'>
+            <Link
+              className='text-cb-yellow hover:text-cb-yellow/75 disabled:pointer-events-none disabled:opacity-25'
+              to='new'
+            >
+              <PencilSquareIcon className='h-6 w-6' />
+            </Link>
+          </div>
         </div>
       </footer>
     </>
