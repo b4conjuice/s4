d88ff58f938487c76e37e1feeb51db0fdb4f212f
Daniel Lopez <d@niel.to>
2026-08-21 15:44:09 -0700

    feat(scripture-list): add button to open scripture

--- src/components/scripture-list.tsx
+++ src/components/scripture-list.tsx
@@ -1,8 +1,11 @@
 import { NavLink as Link } from 'react-router'
+import { ArrowTopRightOnSquareIcon } from '@heroicons/react/20/solid'
 
 import { transformTextToScripture } from '@/lib/books'
+import useOpenScriptureUrl from '@/lib/useOpenScriptureUrl'
 
 export default function ScriptureList({ list }: { list: string[] }) {
+  const openScriptureUrl = useOpenScriptureUrl()
   return (
     <ul className='divide-cb-dusty-blue divide-y'>
       {list.map((bibleParam, index) => {
@@ -31,6 +34,15 @@ export default function ScriptureList({ list }: { list: string[] }) {
                 <div>{scripture.asString}</div>
               </div>
             </Link>
+            <button
+              className='text-cb-pink hover:text-cb-pink/75 disabled:pointer-events-none disabled:opacity-25'
+              type='button'
+              onClick={() => {
+                openScriptureUrl(scripture)
+              }}
+            >
+              <ArrowTopRightOnSquareIcon className='h-6 w-6' />
+            </button>
           </li>
         )
       })}
